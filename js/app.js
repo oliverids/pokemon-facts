c9dbb3be99a8ff2851c69c1a5143a5f4cfadde30
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-12-10 21:07:38 -0300

    skills list function

--- js/app.js
+++ js/app.js
@@ -19,14 +19,24 @@ export default function App(pokemon) {
                 spdefenseNumber = document.getElementById('spdefense-number'),
                 spdefenseBar = document.getElementById('spdefense'),
                 speedNumber = document.getElementById('speed-number'),
-                speedBar = document.getElementById('speed');
+                speedBar = document.getElementById('speed'),
+                //skills and moves
+                skillList = document.getElementById('skill-list'),
+                movesList = document.getElementById('moves-list');
 
-            types.innerHTML = '';
+
+            //reset
+            function resetChildren(e) {
+                e.innerHTML = ''
+            }
+            [types,skillList, movesList].forEach(resetChildren)
+            // types.innerHTML = '';
 
             function capitalize(str) {
                 return str.charAt(0).toUpperCase() + str.slice(1);
             }
 
+            //fotos, tipos e nome
             front.src = r.sprites.front_default;
             back.src = r.sprites.back_default;
             r.sprites.back_default ? back.src = r.sprites.back_default : back.style.display = 'none';
@@ -39,10 +49,10 @@ export default function App(pokemon) {
                 types.appendChild(tipos);
             }
 
+            //base stats
             function progressBar(valor, number, total) {
                 number.innerText = valor;
                 total.style.width = `${(100 * valor) / total.parentElement.offsetWidth}%`;
-                // console.log((100 * valor) / total.parentElement.offsetWidth)
             }
 
             progressBar(r.stats[0].base_stat, healthNumber, healthBar);
@@ -51,5 +61,30 @@ export default function App(pokemon) {
             progressBar(r.stats[2].base_stat, defenseNumber, defenseBar);
             progressBar(r.stats[4].base_stat, spdefenseNumber, spdefenseBar);
             progressBar(r.stats[5].base_stat, speedNumber, speedBar);
+            
+            //skills list
+            for (let i = 0; i < 2; i++) {
+                if (i !== r.abilities.length) {
+                    let item = document.createElement('li'),
+                        titulo = document.createElement('h2'),
+                        efeito = document.createElement('p'),
+                        shortEfeito = document.createElement('p');
+
+                    titulo.innerText = r.abilities[i].ability.name.replace('-', ' ').split(' ').map(capitalize).join(' ');
+
+                    fetch(r.abilities[i].ability.url).then(r => r.json())
+                        .then(r => {
+                            efeito.innerText = r.effect_entries[1].effect.replace(/(?:\r\n|\r|\n)/g, ' ');
+                            shortEfeito.innerText = capitalize(r.effect_entries[1].short_effect)
+                        })
+
+                    function append(e) {
+                        item.append(e)
+                    }
+
+                    [titulo, efeito, shortEfeito].forEach(append)
+                    skillList.appendChild(item)
+                } else { break }
+            }
         })
 }
