bace0b81194aad0a2dd33f278cb4df19f3672f6f
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-12-12 16:16:01 -0300

    localstorage theme variable

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 import App from './app.js';
 import Randomize from './randomize.js';
 Randomize();
-App('pikachu')
+
 const theme = document.getElementById('theme');
 theme.addEventListener('click', () => {
     document.body.classList.toggle('escuro');
     theme.classList.toggle('ativo');
+
+    document.body.classList.contains('escuro') ? localStorage.setItem('tema', 'escuro') : localStorage.setItem('tema', 'claro');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('tema') == 'claro') {
+        document.body.classList.remove('escuro');
+        theme.classList.remove('ativo');
+    } else {
+        document.body.classList.add('escuro');
+        theme.classList.add('ativo');
+    }
 })
 
 const menu = document.getElementById('menu'),
